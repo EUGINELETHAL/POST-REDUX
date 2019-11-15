5a383a556024a7ea828edbce036b154a5f1f4409
EUGINELETHAL <ochungeugine@gmail.com>
2019-11-15 17:01:27 +0300

    updated redux

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Posts from "./components/Posts";
+
+
+
+function App() {
+  return (
+    
+
+    <div className="App">
+      <Posts/>
+    </div>
+    
+  );
+}
+
+export default App;
